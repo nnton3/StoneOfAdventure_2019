2751538072b12348491b3e57a5ed7232a1d888ea
Anton <34330466+nnton3@users.noreply.github.com>
2019-10-11 09:25:11 +0300

    work with unit states

---
+++ StoneOfAdventure_2019_UnityProject/Assets/Scripts/States/IdleState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using StoneOfAdventure.Movement;
+using System;
+
+public class IdleState : MonoBehaviour, IUnitState
+{
+    Mover mover;
+
+    private void Start()
+    {
+        mover = GetComponent<Mover>();
+    }
+
+    public void Attack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Idle()
+    {
+        return;
+    }
+
+    public void Jump()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MoveHorizontal(float direction, float movespeed)
+    {
+        mover.MoveTo(direction, movespeed);
+    }
+
+    public void MoveVertical()
+    {
+        throw new NotImplementedException();
+    }
+}
--- StoneOfAdventure_2019_UnityProject/Assets/Units/Player/PlayerController.cs
+++ StoneOfAdventure_2019_UnityProject/Assets/Units/Player/PlayerController.cs
@@ -3,5 +3,45 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    [SerializeField] private float movespeed;
+
+    private void Start()
+    {
+        State = GetComponent<IdleState>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0f) MoveHorizontal(Input.GetAxisRaw("Horizontal"));
+        if (Input.GetAxisRaw("Vertical") != 0f) MoveVertical();
+        if (Input.GetAxisRaw("Attack") != 0f) Attack();
+        if (Input.GetAxisRaw("Jump") != 0f) Jump();
+    }
+
+    public IUnitState State { get; set; }
+
+    private void Idle()
+    {
+        State.Idle();
+    }
+
+    private void MoveHorizontal (float direction)
+    {
+        State.MoveHorizontal(direction, movespeed);
+    }
+
+    private void MoveVertical()
+    {
+        State.MoveVertical();
+    }
+
+    private void Attack()
+    {
+        State.Attack();
+    }
+
+    private void Jump()
+    {
+        State.Jump();
+    }
 }
