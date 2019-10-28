5df62439fc007ece9b0ff54ad787436788ab4589
Anton <34330466+nnton3@users.noreply.github.com>
2019-10-28 21:49:49 +0300

    test attacker

---
+++ StoneOfAdventure_2019_UnityProject/Assets/Scripts/Combat/EnemieFactory.cs
@@ -0,0 +1,37 @@
+﻿using StoneOfAdventure.Core;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class EnemieFactory : MonoBehaviour
+{
+    private Tilemap groundTilemap;
+    private Collider2D targetCol;
+
+    private void Start()
+    {
+        groundTilemap = GameObject.FindGameObjectWithTag("Ground").GetComponent<Tilemap>();
+    }
+
+    private void SpawnEnemie()
+    {
+        Vector3 spawnPosition = ChangeSpawnPosition();
+        GameObject enemie = ChangeEnemie();
+
+    }
+
+    private Vector3 ChangeSpawnPosition()
+    {
+        BoundsInt newB = new BoundsInt();
+        
+        TileBase[] groundTiles = groundTilemap.GetTilesBlock(newB);
+
+        return Vector3.zero;
+    }
+
+    private GameObject ChangeEnemie()
+    {
+        throw new NotImplementedException();
+    }
+}
