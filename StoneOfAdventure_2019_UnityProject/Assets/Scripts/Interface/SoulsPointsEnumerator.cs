15fa988140d7a06b24971c9f8cbf35623b9eaa5c
Anton <34330466+nnton3@users.noreply.github.com>
2019-11-10 17:57:33 +0300

    fix ui

--- StoneOfAdventure_2019_UnityProject/Assets/Scripts/Interface/SoulsPointsEnumerator.cs
+++ StoneOfAdventure_2019_UnityProject/Assets/Scripts/Interface/SoulsPointsEnumerator.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SoulsPointsEnumerator : MonoBehaviour
 {
-    private Text soulsPoints;
+    private TextMeshProUGUI soulsPoints;
     private Treasury treasury;
 
     private void Start()
     {
-        soulsPoints = GetComponent<Text>();
+        soulsPoints = GetComponent<TextMeshProUGUI>();
         treasury = FindObjectOfType<Treasury>();
 
         treasury.TresureUpdated.AddListener(UpdateSoulsEnumerator);
