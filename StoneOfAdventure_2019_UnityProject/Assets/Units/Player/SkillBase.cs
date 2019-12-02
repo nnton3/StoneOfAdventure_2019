7ef93d6cb3e27d4b55d9de3e15fb03cd36a6bfea
Anton <34330466+nnton3@users.noreply.github.com>
2019-12-02 20:23:33 +0300

    Add timer to skills icons

--- StoneOfAdventure_2019_UnityProject/Assets/Units/Player/SkillBase.cs
+++ StoneOfAdventure_2019_UnityProject/Assets/Units/Player/SkillBase.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Events;
 
 public class SkillBase : MonoBehaviour
 {
     [SerializeField] private float coolDown = 2f;
+    public float CoolDown => coolDown;
     [SerializeField] protected int baseDamage = 10;
 
     private bool canUseSkill = true;
     public bool CanUseSkill => canUseSkill;
 
+    public UnityEvent SkillUsed;
+
     public virtual void StartUse()
     {
         canUseSkill = false;
         StartCoroutine("CoolDownTimer");
+        SkillUsed.Invoke();
     }
 
     public void IncreaseBaseDamage(float increaseDamage)
