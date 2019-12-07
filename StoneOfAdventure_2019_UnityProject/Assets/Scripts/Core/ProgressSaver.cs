f0f77dc283d89be94c47fb247ea3d5c387b198ea
Anton <34330466+nnton3@users.noreply.github.com>
2019-12-07 12:35:28 +0300

    add scene for test scene transitions
    add base logic for scene transitions

---
+++ StoneOfAdventure_2019_UnityProject/Assets/Scripts/Core/ProgressSaver.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace StoneOfAdventure.Core
+{
+    public class ProgressSaver : MonoBehaviour
+    {
+        [SerializeField] private GameObject followCamera;
+        private Transform player;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+            player = FindObjectOfType<PlayerStateController>().transform;
+            player.transform.position = Vector3.zero;
+        }
+
+        public void InstanceNecessaryPrefs()
+        {
+            var followCameraInstance = Instantiate(followCamera, Vector3.zero, Quaternion.identity);
+            followCameraInstance.GetComponent<Cinemachine.CinemachineVirtualCamera>().m_Follow = player;
+        }
+    }
+}
---
+++ StoneOfAdventure_2019_UnityProject/Assets/Scripts/Core/SceneSwapper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace StoneOfAdventure.Core
+{
+    public class SceneSwapper : MonoBehaviour
+    {
+        [SerializeField] private int activatedSceneNumber;
+
+        public void SwapScene()
+        {
+            SceneManager.LoadScene(activatedSceneNumber);
+        }
+    }
+}
