f184f54e6402f4ed6116d7f935ed8e947b666a7a
Anton <34330466+nnton3@users.noreply.github.com>
2019-12-23 19:48:48 +0300

    add paladin fighter and state controller

---
+++ StoneOfAdventure_2019_UnityProject/Assets/Units/Paladin/PaladinFighter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using StoneOfAdventure.Combat;
+using System;
+
+namespace StoneOfAdventure.Combat
+{
+    public class PaladinFighter : Fighter
+    {
+        private Transform player;
+
+        [SerializeField] private float meleeRange;
+
+        protected override void Start()
+        {
+            base.Start();
+            player = FindObjectOfType<PaladinStateController>().transform;
+        }
+
+        public override void StartAttack()
+        {
+            if (TargetInMelee()) StartMeleeAttack();
+
+            StartRangeAttack();
+        }
+
+        private void StartRangeAttack()
+        {
+            Attack?.Invoke();
+            anim.SetTrigger("rangeAttack");
+        }
+
+        private void StartMeleeAttack()
+        {
+            Attack?.Invoke();
+            anim.SetTrigger("meleeAttack");
+        }
+
+        private bool TargetInMelee()
+        {
+            return (player.position.x - transform.position.x) <= meleeRange;
+        }
+    }
+}
--- StoneOfAdventure_2019_UnityProject/Assets/Units/Paladin/PaladinStateController.cs
+++ StoneOfAdventure_2019_UnityProject/Assets/Units/Paladin/PaladinStateController.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
+﻿using StoneOfAdventure.Combat;
+using StoneOfAdventure.Core;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PaladinStateController : MonoBehaviour
+public class PaladinStateController : Unit
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform player;
+    private Fighter fighter;
+
+    private void Start()
     {
-        
+        player = FindObjectOfType<PlayerStateController>().transform;
+        fighter = GetComponent<Fighter>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (currentState == State.Death) return;
+        if (player) Attack();
+    }
+
+    #region Events
+    public override void Attack()
+    {
+        if (currentState == State.Idle)
+        {
+            StateAttack();
+            fighter.StartAttack();
+        }
+    }
+    #endregion
+
+    #region Transitions
+    private void StateAttack()
+    {
+        SetState(State.Attack);
+    }
+    #endregion
+
+    private void SetState(State value)
+    {
+        currentState = value;
     }
 }
