a4d4be8c30e3c0328c8003c1415772a94875591b
Anton <34330466+nnton3@users.noreply.github.com>
2020-02-13 19:28:15 +0300

    update main canvas and paladin

--- StoneOfAdventure_2019_UnityProject/Assets/Units/Paladin/PaladinFighter.cs
+++ StoneOfAdventure_2019_UnityProject/Assets/Units/Paladin/PaladinFighter.cs
@@ -5,9 +5,12 @@ using System;
 
 namespace StoneOfAdventure.Combat
 {
-    public class PaladinFighter : RaycastFighter
+    public class PaladinFighter : AOE_Fighter
     {
         private Rigidbody2D rb;
+        [SerializeField] private int damageMelee1;
+        [SerializeField] private int damageMelee2;
+        [SerializeField] private int damageMelee3;
 
         protected override void Start()
         {
@@ -22,15 +25,23 @@ namespace StoneOfAdventure.Combat
             rb.AddForce(Vector2.right * dir * power, ForceMode2D.Impulse);
         }
 
+        public override void StartAttack()
+        {
+            base.StartAttack();
+            baseDamage = damageMelee1;
+        }
+
         public void StartMelee2()
         {
             UseAttack.Invoke();
+            baseDamage = damageMelee2;
             anim.SetTrigger("melee2");
         }
 
         public void StartMelee3()
         {
             UseAttack.Invoke();
+            baseDamage = damageMelee3;
             anim.SetTrigger("melee3");
         }
     }
