da310252ccbf7e7b329266465f3e36a0a36408fc
Anton <34330466+nnton3@users.noreply.github.com>
2020-02-18 19:24:17 +0300

    fix paladin bugs and his health bar

--- StoneOfAdventure_2019_UnityProject/Assets/Units/Paladin/PaladinFighter.cs
+++ StoneOfAdventure_2019_UnityProject/Assets/Units/Paladin/PaladinFighter.cs
@@ -5,10 +5,17 @@ using System;
 
 namespace StoneOfAdventure.Combat
 {
-    public class PaladinFighter : AOE_Fighter
+    public class PaladinFighter : Fighter
     {
+        #region Variables
         private Rigidbody2D rb;
+
+        [Header("MELEE ATTACK 1 SETTINGS")]
         [SerializeField] private int damageMelee1;
+        [SerializeField] private Vector3 melee1AreaCenter;
+        [SerializeField] private Vector3 melee1Area;
+        [SerializeField] private bool melee1AreaVisible;
+
 
         [Header("MELEE ATTACK 2 SETTINGS")]
         [SerializeField] private int damageMelee2;
@@ -21,10 +28,14 @@ namespace StoneOfAdventure.Combat
         [SerializeField] private Vector3 melee3AreaCenter;
         [SerializeField] private Vector3 melee3Area;
         [SerializeField] private bool melee3AreaVisible;
+        [SerializeField] private LayerMask layerMask;
+        #endregion
 
-        protected override void OnDrawGizmos()
+        private void OnDrawGizmos()
         {
-            base.OnDrawGizmos();
+            Gizmos.color = Color.red;
+            if (melee1AreaVisible)
+                Gizmos.DrawWireCube(transform.position + melee1AreaCenter, melee1Area);
             if (melee2AreaVisible)
                 Gizmos.DrawWireCube(transform.position + melee2AreaCenter, melee2Area);
             if (melee3AreaVisible)
@@ -47,21 +58,57 @@ namespace StoneOfAdventure.Combat
         public override void StartAttack()
         {
             base.StartAttack();
-            baseDamage = damageMelee1;
         }
 
         public void StartMelee2()
         {
             UseAttack.Invoke();
-            baseDamage = damageMelee2;
             anim.SetTrigger("melee2");
         }
 
         public void StartMelee3()
         {
             UseAttack.Invoke();
-            baseDamage = damageMelee3;
             anim.SetTrigger("melee3");
+        }
+
+        // Animation event
+        public void MeleeAttack1Hit()
+        {
+            Hit(damageMelee1, melee1AreaCenter, melee1Area);
+        }
+
+        // Animation event
+        public void MeleeAttack2Hit()
+        {
+            Hit(damageMelee2, melee2AreaCenter, melee2Area);
+        }
+
+        // Animation event
+        public void MeleeAttack3Hit()
+        {
+            Hit(damageMelee3, melee3AreaCenter, melee3Area);
+        }
+
+        public void Hit(int baseDamage, Vector3 applicationAreaCenter, Vector3 applicationArea)
+        {
+            var currentDamage = baseDamage;
+            applyDamageModifiers?.Invoke(ref currentDamage);
+            Vector2 centerInRelationUnitDirection =
+                transform.position + applicationAreaCenter * ((flip.isFacingRight) ? 1 : -1);
+            var player = Physics2D.OverlapBox(
+                centerInRelationUnitDirection,
+                applicationArea,
+                0f,
+                layerMask);
+
+            if (player != null)
+            {
+                player.GetComponent<Health>().ApplyDamage(currentDamage);
+                applyEffectsOnTarget?.Invoke(player.gameObject);
+                DamageApplied?.Invoke();
+            }
+            Debug.Log($"player is null? {player == null}");
         }
     }
 }
