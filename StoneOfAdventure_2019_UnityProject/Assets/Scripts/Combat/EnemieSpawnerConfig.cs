9c363aee0ec4a6c492193ae4a3688242c2b884a3
Anton <34330466+nnton3@users.noreply.github.com>
2020-03-11 19:14:24 +0300

    add zenject and datafile for lvl1

---
+++ StoneOfAdventure_2019_UnityProject/Assets/Scripts/Combat/EnemieSpawnerConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(menuName ="Combat")]
+public class EnemieSpawnerConfig : ScriptableObject
+{
+    [SerializeField] private float baseSpawnDelay = 5f;
+    [SerializeField] private float minSpawnDelay = 1f;
+    [SerializeField] private List<string> units = new List<string>();
+    [SerializeField] private List<float> baseSpawnChance = new List<float>();
+    [SerializeField] private List<float> endSpawnChance = new List<float>();
+    [SerializeField] private int totalTickNumber = 40;
+}
